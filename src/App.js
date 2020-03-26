e7b148797c6bf1318247fe0c9cfdcfc4780abf33
shimizur0119 <otsukaresamannsatabasa@gmail.com>
2020-03-26 23:16:59 +0900

    init

--- src/App.js
+++ src/App.js
@@ -1,25 +1,286 @@
-import React from 'react';
-import logo from './logo.svg';
-import './App.css';
+import React from "react";
+import logo from "./logo.svg";
+import useStyles from "./style/style";
+
+import { Button, Box, Grid } from "@material-ui/core";
+import MenuIcon from "@material-ui/icons/Menu";
 
 function App() {
+  const classes = useStyles();
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
+    <>
+      <Box mx={5} className={classes.hb}>
+        <Grid container direction="row" alignItems="center" justify="space-between">
+          <Grid item>
+            <Box className={classes.ht} fontSize="h4.fontSize">
+              シミズのポートフォリオ
+            </Box>
+          </Grid>
+          <Grid item>
+            <Box mr={2} mt={2}>
+
+            <MenuIcon className={classes.hm} fontSize="large" />
+            </Box>
+          </Grid>
+        </Grid>
+      </Box>
+      <Box mt={3}>hello</Box>
+      <Box mt={3}>hello</Box>
+      <Box mt={3}>hello</Box>
+      <Box mt={3}>hello</Box>
+      <Box
+        textAlign="center"
+        style={{ fontFamily: "komadori-mini" }}
+        component="h1"
+      >
+        シミズ リョウタ
+      </Box>
+      <Box
+        textAlign="center"
+        style={{ fontFamily: "komadori-mini" }}
+        component="h1"
+      >
+        シミズ リョウタ
+      </Box>
+      <Box
+        textAlign="center"
+        style={{ fontFamily: "komadori-mini" }}
+        component="h1"
+      >
+        シミズ リョウタ
+      </Box>
+      <Box
+        textAlign="center"
+        style={{ fontFamily: "komadori-mini" }}
+        component="h1"
+      >
+        シミズ リョウタ
+      </Box>
+      <Box
+        textAlign="center"
+        style={{ fontFamily: "komadori-mini" }}
+        component="h1"
+      >
+        シミズ リョウタ
+      </Box>
+      <Box
+        textAlign="center"
+        style={{ fontFamily: "komadori-mini" }}
+        component="h1"
+      >
+        シミズ リョウタ
+      </Box>
+      <Box
+        textAlign="center"
+        style={{ fontFamily: "komadori-mini" }}
+        component="h1"
+      >
+        シミズ リョウタ
+      </Box>
+      <Box
+        textAlign="center"
+        style={{ fontFamily: "komadori-mini" }}
+        component="h1"
+      >
+        シミズ リョウタ
+      </Box>
+      <Box
+        textAlign="center"
+        style={{ fontFamily: "komadori-mini" }}
+        component="h1"
+      >
+        シミズ リョウタ
+      </Box>
+      <Box
+        textAlign="center"
+        style={{ fontFamily: "komadori-mini" }}
+        component="h1"
+      >
+        シミズ リョウタ
+      </Box>
+      <Box
+        textAlign="center"
+        style={{ fontFamily: "komadori-mini" }}
+        component="h1"
+      >
+        シミズ リョウタ
+      </Box>
+      <Box
+        textAlign="center"
+        style={{ fontFamily: "komadori-mini" }}
+        component="h1"
+      >
+        シミズ リョウタ
+      </Box>
+      <Box
+        textAlign="center"
+        style={{ fontFamily: "komadori-mini" }}
+        component="h1"
+      >
+        シミズ リョウタ
+      </Box>
+      <Box
+        textAlign="center"
+        style={{ fontFamily: "komadori-mini" }}
+        component="h1"
+      >
+        シミズ リョウタ
+      </Box>
+      <Box
+        textAlign="center"
+        style={{ fontFamily: "komadori-mini" }}
+        component="h1"
+      >
+        シミズ リョウタ
+      </Box>
+      <Box
+        textAlign="center"
+        style={{ fontFamily: "komadori-mini" }}
+        component="h1"
+      >
+        シミズ リョウタ
+      </Box>
+      <Box
+        textAlign="center"
+        style={{ fontFamily: "komadori-mini" }}
+        component="h1"
+      >
+        シミズ リョウタ
+      </Box>
+      <Box
+        textAlign="center"
+        style={{ fontFamily: "komadori-mini" }}
+        component="h1"
+      >
+        シミズ リョウタ
+      </Box>
+      <Box
+        textAlign="center"
+        style={{ fontFamily: "komadori-mini" }}
+        component="h1"
+      >
+        シミズ リョウタ
+      </Box>
+      <Box
+        textAlign="center"
+        style={{ fontFamily: "komadori-mini" }}
+        component="h1"
+      >
+        シミズ リョウタ
+      </Box>
+      <Box
+        textAlign="center"
+        style={{ fontFamily: "komadori-mini" }}
+        component="h1"
+      >
+        シミズ リョウタ
+      </Box>
+      <Box
+        textAlign="center"
+        style={{ fontFamily: "komadori-mini" }}
+        component="h1"
+      >
+        シミズ リョウタ
+      </Box>
+      <Box
+        textAlign="center"
+        style={{ fontFamily: "komadori-mini" }}
+        component="h1"
+      >
+        シミズ リョウタ
+      </Box>
+      <Box
+        textAlign="center"
+        style={{ fontFamily: "komadori-mini" }}
+        component="h1"
+      >
+        シミズ リョウタ
+      </Box>
+      <Box
+        textAlign="center"
+        style={{ fontFamily: "komadori-mini" }}
+        component="h1"
+      >
+        シミズ リョウタ
+      </Box>
+      <Box
+        textAlign="center"
+        style={{ fontFamily: "komadori-mini" }}
+        component="h1"
+      >
+        シミズ リョウタ
+      </Box>
+      <Box
+        textAlign="center"
+        style={{ fontFamily: "komadori-mini" }}
+        component="h1"
+      >
+        シミズ リョウタ
+      </Box>
+      <Box
+        textAlign="center"
+        style={{ fontFamily: "komadori-mini" }}
+        component="h1"
+      >
+        シミズ リョウタ
+      </Box>
+      <Box
+        textAlign="center"
+        style={{ fontFamily: "komadori-mini" }}
+        component="h1"
+      >
+        シミズ リョウタ
+      </Box>
+      <Box
+        textAlign="center"
+        style={{ fontFamily: "komadori-mini" }}
+        component="h1"
+      >
+        シミズ リョウタ
+      </Box>
+      <Box
+        textAlign="center"
+        style={{ fontFamily: "komadori-mini" }}
+        component="h1"
+      >
+        シミズ リョウタ
+      </Box>
+      <Box
+        textAlign="center"
+        style={{ fontFamily: "komadori-mini" }}
+        component="h1"
+      >
+        シミズ リョウタ
+      </Box>
+      <Box
+        textAlign="center"
+        style={{ fontFamily: "komadori-mini" }}
+        component="h1"
+      >
+        シミズ リョウタ
+      </Box>
+      <Box
+        textAlign="center"
+        style={{ fontFamily: "komadori-mini" }}
+        component="h1"
+      >
+        シミズ リョウタ
+      </Box>
+      <Box
+        textAlign="center"
+        style={{ fontFamily: "komadori-mini" }}
+        component="h1"
+      >
+        シミズ リョウタ
+      </Box>
+      <Box
+        textAlign="center"
+        style={{ fontFamily: "komadori-mini" }}
+        component="h1"
+      >
+        シミズ リョウタ
+      </Box>
+    </>
   );
 }
 
---
+++ src/style/style.js
@@ -0,0 +1,17 @@
+import { makeStyles } from "@material-ui/styles";
+
+const useStyles = makeStyles(() => ({
+  ht: {
+    fontFamily: "komadori-mini",
+    zIndex: 100,
+    position: "fixed"
+  },
+  hm: {
+    zIndex: 100,
+    position: "fixed"
+  },
+  hb: {
+  }
+}));
+
+export default useStyles;
